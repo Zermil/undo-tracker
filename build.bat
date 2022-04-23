764b94fdbc577e29329f183a097961afba60f4c8
Aiden <marcin.madry@interia.com>
2022-04-23 23:49:52 +0200

    Change build

--- build.bat
+++ build.bat
@@ -8,5 +8,5 @@ set MSVC_PATH="YOUR_PATH"
 call %MSVC_PATH%\vcvars64.bat
 
 pushd %MY_PATH%\build
-cl ..\src\main.cpp /I../deps/SDL2/include /std:c++17 /Fe:undo-tracker.exe /Zi /EHsc /link ..\deps\SDL2\lib\SDL2.lib ..\deps\SDL2\lib\SDL2main.lib 
+cl ..\src\main.cpp /I../deps/SDL2/include /std:c++17 /Fe:undo-tracker.exe /Zi /EHsc /link ..\deps\SDL2\lib\SDL2.lib ..\deps\SDL2\lib\SDL2main.lib ..\deps\SDL2\lib\SDL2_ttf.lib
 popd
