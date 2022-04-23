50b2ab766053baed44922bbc1c25ad8c2bcaa8ec
Aiden <marcin.madry@interia.com>
2022-04-23 21:44:55 +0200

    Change building

--- build.bat
+++ build.bat
@@ -1,11 +1,12 @@
 @echo off
 
+set MY_PATH=%~dp0
+
 REM Change this to your visual studio's 'vcvars64.bat' script path
 set MSVC_PATH="YOUR_PATH"
 
 call %MSVC_PATH%\vcvars64.bat
-    
-if not exist .\build mkdir build
-pushd .\build
-cl ..\main.cpp /std:c++17 /Fe:undo-tracker.exe /Zi /EHsc
+
+pushd %MY_PATH%\build
+cl ..\src\main.cpp /I../deps/SDL2/include /std:c++17 /Fe:undo-tracker.exe /Zi /EHsc /link ..\deps\SDL2\lib\SDL2.lib ..\deps\SDL2\lib\SDL2main.lib 
 popd
