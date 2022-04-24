699cd3b760c4fbcfef8e639b11b8c9bf0fd4d82d
Aiden <marcin.madry@interia.com>
2022-04-24 17:40:05 +0200

    Change build to be more customizable and, by extension, slightly more sane

--- build.bat
+++ build.bat
@@ -4,9 +4,13 @@ set MY_PATH=%~dp0
 
 REM Change this to your visual studio's 'vcvars64.bat' script path
 set MSVC_PATH="YOUR_PATH"
-
 call %MSVC_PATH%\vcvars64.bat
 
 pushd %MY_PATH%\build
-cl ..\src\main.cpp /I../deps/SDL2/include /std:c++17 /Fe:undo-tracker.exe /Zi /EHsc /link ..\deps\SDL2\lib\SDL2.lib ..\deps\SDL2\lib\SDL2main.lib ..\deps\SDL2\lib\SDL2_ttf.lib /SUBSYSTEM:windows /ENTRY:mainCRTStartup
+
+set SDL2_PATH="..\deps\SDL2"
+set SDL2_LINK="%SDL2_PATH%\lib\SDL2.lib" "%SDL2_PATH%\lib\SDL2main.lib" "%SDL2_PATH%\lib\SDL2_ttf.lib"
+
+cl ..\src\main.cpp /I%SDL2_PATH%\include /std:c++17 /EHsc %* /Fe:undo-tracker.exe /link %SDL2_LINK% /SUBSYSTEM:windows /ENTRY:mainCRTStartup
+
 popd
